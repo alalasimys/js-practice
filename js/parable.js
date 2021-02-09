f1317b342e570b06dba4fe0420f033ca5973702c
Alla Karhopolova <alalasimys@gmail.com>
2021-02-09 23:09:23 +0200

    примеры

---
+++ js/parable.js
@@ -0,0 +1,12 @@
+let arr = [87, 3, 4, 0, 1, 4, 5];
+let parable = [];
+let strArr = arr.join(",");
+console.log(strArr);
+let topParable = Math.max(strArr);
+console.log(topParable);
+for (let i = 0; i < arr.length; i++) {}
+
+//отсортирповать массив в порядке убывания
+// макс элемент поставить на средний индекс
+// расставлять элементы вокруг макс элемента по убыванию
+
