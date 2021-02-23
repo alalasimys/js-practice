5f1ccae82f590980880770a95fd20f6a281b5231
Alla Karhopolova <alalasimys@gmail.com>
2021-02-24 00:19:16 +0200

    add cw task

--- js/cw-4-7kyu.js
+++ js/cw-4-7kyu.js
@@ -1,1 +1,12 @@
+function calculateYears(principal, interest, tax, desired) {
+  let years = 0;
 
+  while (principal < desired) {
+    principal = principal * (1 + interest) - principal * interest * tax;
+    years += 1;
+  }
+  return years;
+}
+console.log(calculateYears(1000, 0.05, 0.18, 1100));
+console.log(calculateYears(1000, 0.01625, 0.18, 1200));
+console.log(calculateYears(1000, 0.05, 0.18, 1000));
