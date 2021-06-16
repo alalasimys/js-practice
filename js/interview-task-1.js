4414a34d89d00d20156c89febab8ef24e2d15074
Alla Karhopolova <alalasimys@gmail.com>
2021-06-16 04:22:22 +0300

    add cd task

--- js/interview-task-1.js
+++ js/interview-task-1.js
@@ -29,6 +29,8 @@ const fruits = [
 // }
 // console.log(obj);
 
+//2
+/*
 console.log(
   fruits.reduce((obj, fruit) => {
     if (!obj[fruit]) {
@@ -39,3 +41,34 @@ console.log(
     return obj;
   }, {})
 );
+*/
+
+//3
+
+function reverseNumber(n) {
+  return parseFloat(n.toString().split("").reverse().join("")) * Math.sign(n);
+}
+
+//4
+
+function getStrings(city) {
+  let cityObj = city
+    .trim()
+    .replace(" ", "")
+    .toLowerCase()
+    .replace(/[^a-zA-Z]+/g, "")
+    .split("")
+    .reduce((obj, letter) => {
+      if (!obj[letter]) {
+        obj[letter] = "";
+      }
+      obj[letter] += "*";
+      return obj;
+    }, {});
+  cityObj = JSON.stringify(cityObj).replace(/[{}"]/g, "");
+  return cityObj;
+}
+
+console.log(getStrings("Chicago"));
+console.log(getStrings("Bangkok"));
+console.log(getStrings("Las Vegas"), "l:*,a:**,s:**,v:*,e:*,g:*");
